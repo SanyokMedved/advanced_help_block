60eb753de3edb3ddd39e59434b799998428c16f7
Oleksandr Medvediev <oleksandrm@imagexmedia.com>
2018-05-03 13:42:57 +0300

    Master: add advanced_help_block module.

---
+++ src/Form/AdvancedHelpBlockDeleteForm.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\advanced_help_block\Form;
+
+use Drupal\Core\Entity\ContentEntityConfirmFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Provides a form for deleting a advanced_help_block entity.
+ *
+ * @ingroup advanced_help_block
+ */
+class AdvancedHelpBlockDeleteForm extends ContentEntityConfirmFormBase {
+
+  /**
+   * Returns the question to ask the user.
+   *
+   * @return string
+   *   The form question. The page title will be set to this value.
+   */
+  public function getQuestion() {
+    return t('Are you sure you want delete this entity?');
+  }
+
+  /**
+   * Returns the route to go to if the user cancels the action.
+   *
+   * @return \Drupal\Core\Url
+   *   A URL object.
+   */
+  public function getCancelUrl() {
+    return new Url('view.advanced_help_blocks.page_1');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getConfirmText() {
+    return $this->t('Delete');
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Delete the entity and log the event. log() replaces the watchdog.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    //this method is the submit handler for our form
+
+    $entity = $this->getEntity();
+    $entity->delete();
+
+    \Drupal::logger('advanced_help_block')->notice(
+      '@type: deleted %title.',
+      array(
+        '@type' => $this->entity->bundle(),
+        '%title' => $this->entity->get('field_ahb_title')->value,
+      )
+    );
+
+    //redirect to the
+    $form_state->setRedirect('view.advanced_help_blocks.page_1');
+  }
+}
